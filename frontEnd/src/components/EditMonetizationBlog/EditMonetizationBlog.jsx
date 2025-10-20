72cc45b24ea242355263f4efcf741a0801fd0dd7
vineet  patil <vineet.v88888@gmail.com>
2025-10-20 18:08:39 +0530

    Update API URL for monetization blog component

--- frontEnd/src/components/EditMonetizationBlog/EditMonetizationBlog.jsx
+++ frontEnd/src/components/EditMonetizationBlog/EditMonetizationBlog.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const API = 'http://localhost:5000';
+const API = 'https://blog-web-with-admin-panel.onrender.com';
 
 function EditMonetizationBlog() {
   const [formData, setFormData] = useState({ EditMonetizationBlog: '' });
