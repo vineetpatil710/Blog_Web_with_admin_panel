cd68c9236111c4be7ac66b9e86bfe4dd0f56ec4b
vineet  patil <vineet.v88888@gmail.com>
2025-10-20 18:07:45 +0530

    Update API_URL to production endpoint

--- frontEnd/src/components/EditHeaderColor/EditHeaderColor.jsx
+++ frontEnd/src/components/EditHeaderColor/EditHeaderColor.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000';
+const API_URL = 'https://blog-web-with-admin-panel.onrender.com';
 
 function EditHeaderColor() {
   const [selectedColor, setSelectedColor] = useState('#ffffff');
