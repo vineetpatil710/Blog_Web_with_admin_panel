6609af7613b0c2b8aa411892c7ed29e57c88905b
vineet  patil <vineet.v88888@gmail.com>
2025-10-30 17:02:07 +0530

    Update index.js

--- frontEnd/src/index.js
+++ frontEnd/src/index.js
@@ -13,7 +13,11 @@ root.render(
     
   </React.StrictMode>
 );
-
+//
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+const path = require('path');
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
